d247a0dcd50eea15f690cf69cfe41e86a6362e59
Trevor Coleman <trevor@trevorcoleman.design>
2023-10-10 14:54:56 -0400

    remove duplicate nested docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,10 +23,10 @@ const config = {
   trailingSlash: true,
 
   // Set the production url of your site here
-  url: 'https://infinitered.github.io',
+  url: 'https://docs.infinite.red',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/ir-docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
