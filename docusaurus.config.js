9239348a5233039ed217245a5b23e0bfa64aa890
Trevor Coleman <trevor@trevorcoleman.design>
2023-10-11 16:37:22 -0400

    change social preview image

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -68,7 +68,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/irVertical.jpg',
       navbar: {
         title: 'Open Source @ Infinite Red',
         logo: {
