4fbef77049930276ac2f463a523df126d1168ed3
Trevor Coleman <trevor@trevorcoleman.design>
2023-10-11 16:14:08 -0400

    fix broken docs links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -89,15 +89,6 @@ const config = {
         style: 'dark',
         links: [
           {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
             title: 'Community',
             items: [
               {
