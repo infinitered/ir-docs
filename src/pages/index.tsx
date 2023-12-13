43b57c6caaa4d380f605ee8fbf872440fb801cb2
Trevor Coleman <trevor@trevorcoleman.design>
2023-12-13 10:37:29 -0500

    style(homepage): Update styles based on designer feedback (#28)
    * changed flex to grid
    * used vars for colours instead of actual colors
    * set default font
    * toned down dark mode
    * added spacing
    
    ![CleanShot 2023-12-11 at 17 37
    17@2x](https://github.com/infinitered/ir-docs/assets/22041394/a0a7ebd9-df88-4de0-bb0d-85f435df9c1a)
    
    
    ![CleanShot 2023-12-11 at 17 37
    48@2x](https://github.com/infinitered/ir-docs/assets/22041394/d905e12b-04a7-4ea4-af44-756bddf75631)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomePageProjects from '@site/src/components/HomePageProjects';
@@ -12,7 +11,7 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-          <img src={'img/irMark--reversed@2x.png'} alt={'Infinite Red Logo'} className={styles.logo}/>
+          <img src={'img/logo.svg'} alt={'Infinite Red Logo'} className={styles.logo}/>
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
       </div>
@@ -24,8 +23,8 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={siteConfig.title}
+      description="Docs for Open Source Projects from Infinite Red, including Reactotron, react-native-mlkit, gluegun and more">
       <HomepageHeader />
       <main>
         <HomePageProjects />
